f40185a50e7e2f10b81360963001a6fca4a8ef62
Logen11fingers <126648995+Logen10fingers@users.noreply.github.com>
2025-05-12 11:25:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function firstWord(s) {
-  // your code here
+  // Trim leading spaces, then split and return first segment
+  s = s.trim();
+  const index = s.indexOf(' ');
+  if (index === -1) return s;
+  return s.substring(0, index);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
